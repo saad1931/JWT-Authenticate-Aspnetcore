7b9b20af9403b3d4b8b562f3da93430c6ca278b6
Muhammad Saad <muhammadsaad3119@gmail.com>
2021-09-23 00:36:24 +0500

    Add project files.

---
+++ JWT-Authorization/Controllers/NameController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWT_Authorization.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJWTAuthenticationManager jWTAuthenticationManager;
+        public NameController(IJWTAuthenticationManager jWTAuthenticationManager)
+        {
+            this.jWTAuthenticationManager = jWTAuthenticationManager;
+        }
+        // GET: api/<NameController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Pakistan", "Iran" };
+        }
+
+        // GET api/<NameController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred UserCred)
+        {
+            var token = jWTAuthenticationManager.AUthenticate
+                (UserCred.username, UserCred.password);
+            if (token == null)
+                return Unauthorized();
+            return Ok(token);
+        }
+
+    }
+}
